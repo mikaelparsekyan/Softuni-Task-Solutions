e54b05f08d30d3d4862a81cc30354d3361f896ea
speeddemon <mikael90@abv.bg>
2019-08-09 11:44:58 +0300

    Added new task

---
+++ Java Advanced/Stack and Queues/src/MatchingBrackets.java
@@ -0,0 +1,19 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String expression = scanner.nextLine();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < expression.length(); i++) {
+            char currentChar = expression.charAt(i);
+            if ('(' == currentChar) {
+                stack.push(i);
+            } else if(')'==currentChar){
+                System.out.println(expression.substring(stack.pop(),i + 1));
+            }
+        }
+    }
+}
