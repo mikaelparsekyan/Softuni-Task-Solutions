f50aafceb0e31d3dc8537e1d41305daaf9af01e8
speeddemon <mikael90@abv.bg>
2019-09-22 10:47:26 +0300

    Fixed code at SimpleTextEditor.java

--- Java Advanced/Stack and Queues/src/SimpleTextEditor.java
+++ Java Advanced/Stack and Queues/src/SimpleTextEditor.java
@@ -11,15 +11,15 @@ public class SimpleTextEditor {
             String[] elements = scanner.nextLine().split(" ");
             switch (elements[0]) {
                 case "1":
-                    text.append(elements[1]);
                     stack.push(text.toString());
+                    text.append(elements[1]);
                     break;
                 case "2":
                     int count = Integer.parseInt(elements[1]);
                     if(count<= text.toString().length()) {
+                        stack.push(text.toString());
                         text = new StringBuilder(text.substring(0, text.length() - count));
                     }
-                    stack.push(text.toString());
                     break;
                 case "3":
                     int index = Integer.parseInt(elements[1]);
@@ -28,12 +28,6 @@ public class SimpleTextEditor {
                     }
                     break;
                 case "4":
-                    //if(!stack.isEmpty()) {
-                    assert stack.peek() != null;
-                    if (stack.peek().contentEquals(text)) {
-                        stack.pop();
-                    }
-                    //}
                     if(!stack.isEmpty()){
                         text = new StringBuilder(stack.pop());
                     }
