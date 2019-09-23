4e17b173a2c26bdbbb1c77d6552f652a798da20a
speeddemon <mikael90@abv.bg>
2019-09-23 14:54:24 +0300

    Fixed code at Robotics.java

--- Java Advanced/Stack and Queues/src/Robotics.java
+++ Java Advanced/Stack and Queues/src/Robotics.java
@@ -6,46 +6,54 @@ public class Robotics {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         //ROB-15;SS2-10;NX8000-3
-        String[] input = scanner.nextLine().split(";");
-        int len = input.length;
-
-        String[] robotNames = new String[len];
-        int[] itemTimes = new int[len];
-        int[] workingTime = new int[len];
-
-        for (int i = 0; i < len; i++) {
-            String[] elements = input[i].split("-");
-            robotNames[i] = elements[0];
-            itemTimes[i] = Integer.parseInt(elements[1]);
-        }
-        int[] time = Arrays.stream(scanner.nextLine()
-                .split(":")).mapToInt(Integer::parseInt).toArray();
-        LocalTime localTime = LocalTime.of(time[0], time[1], time[2]);
-        String product = scanner.nextLine();
+        String[] inputRobots = scanner.nextLine().split(";");
         ArrayDeque<String> queue = new ArrayDeque<>();
+        Map<String, Integer> robots = new LinkedHashMap<>();
+        int[] robotsProcessTime = new int[inputRobots.length];
+
+        for (int i = 0; i < inputRobots.length; i++) {
+            String[] robotsAndTimes = inputRobots[i].split("-");
+            robots.put(robotsAndTimes[0], Integer.parseInt(robotsAndTimes[1]));
+        }
+        int[] time = Arrays.stream(scanner.nextLine().split(":"))
+                .mapToInt(Integer::parseInt).toArray();
+        LocalTime localTime = LocalTime.of(time[0],time[1],time[2]);
+
+        String product = scanner.nextLine();
+
         while (!"End".equals(product)) {
             queue.offer(product);
             product = scanner.nextLine();
         }
+        long timeSec = 0;
         while (!queue.isEmpty()) {
-            localTime = localTime.plusSeconds(1);
             String item = queue.poll();
-            boolean isFree = true;
-            for (int i = 0; i < robotNames.length; i++) {
-                if (workingTime[i] == 0 && isFree) {
-                    isFree = false;
-                    workingTime[i] = itemTimes[i];
-                    System.out.printf("%s - %s [%02d:%02d:%02d]\n", robotNames[i], item, localTime.getHour(),
-                            localTime.getMinute(), localTime.getSecond());
+            localTime = localTime.plusSeconds(1);
+            for (int i = 0; i < robotsProcessTime.length; i++) {
+                if (robotsProcessTime[i] > 0) {
+                    robotsProcessTime[i]--;
                 }
 
-                if (workingTime[i] > 0) {
-                    workingTime[i]--;
-                }
             }
-            if (isFree) {
+            boolean isFree = true;
+            int i = 0;
+            for (var robot : robots.entrySet()) {
+                if (robotsProcessTime[i] == 0) {
+                    robotsProcessTime[i] = robot.getValue();
+                    printResult(robot.getKey(),item,localTime);
+                    isFree = false;
+                    break;
+                }
+                i++;
+            }
+            if(isFree){
                 queue.offer(item);
             }
+
         }
+    }
+
+    private static void printResult(String robot, String item, LocalTime time) {
+        System.out.printf("%s - %s [%02d:%02d:%02d]%n",robot,item,time.getHour(),time.getMinute(),time.getSecond());
     }
 }
