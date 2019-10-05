f7827717f4419ee1c906e701d5dc46feb2a04991
speeddemon <mikael90@abv.bg>
2019-10-05 17:06:59 +0300

    Added SetsOfElements.java

---
+++ Java Advanced/Sets And Maps Advanced/src/SetsOfElements.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class SetsOfElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] params = Arrays.stream(scanner.nextLine()
+                .split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        Set<Integer> firstSet = new LinkedHashSet<>();
+        Set<Integer> secondSet = new LinkedHashSet<>();
+        int n = params[0] + params[1];
+        for (int i = 0; i < n; i++) {
+            int value = Integer.parseInt(scanner.nextLine());
+            if (i < params[0]) {
+                firstSet.add(value);
+            } else {
+                secondSet.add(value);
+            }
+        }
+        for (Integer element : firstSet) {
+            if(secondSet.contains(element)){
+                System.out.print(element + " ");
+            }
+        }
+
+
+    }
+}
