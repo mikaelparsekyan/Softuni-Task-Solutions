a46e0e5644360901ea34164d86f77e859741bdef
speeddemon <mikael90@abv.bg>
2019-10-05 18:55:07 +0300

    Added AMinerTask.java

---
+++ Java Advanced/Sets And Maps Advanced/src/AMinerTask.java
@@ -0,0 +1,23 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AMinerTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String material = scanner.nextLine();
+        Map<String, Integer> materials = new LinkedHashMap<>();
+        while (!"stop".equals(material)) {
+            int quantity = Integer.parseInt(scanner.nextLine());
+            if (materials.containsKey(material)) {
+                materials.put(material,materials.get(material) + quantity);
+            } else {
+                materials.put(material, quantity);
+            }
+            material = scanner.nex  tLine();
+        }
+        materials.entrySet().stream().forEach(e -> {
+            System.out.printf("%s -> %d%n", e.getKey(), e.getValue());
+        });
+    }
+}
