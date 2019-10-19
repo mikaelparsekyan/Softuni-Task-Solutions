20e69709379c4bb1750bfea32a31ca62f1d67a20
speeddemon <mikael90@abv.bg>
2019-10-19 18:16:57 +0300

    Added CustomList task

---
+++ Java Advanced/Generics/src/custom_list/CustomList.java
@@ -0,0 +1,61 @@
+package custom_list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomList<T extends Comparable> {
+    private List<T> data;
+
+    CustomList() {
+        this.data = new ArrayList<>();
+    }
+
+    void add(T element) {
+        this.data.add(element);
+    }
+
+    T remove(int index) {
+        return this.data.remove(index);
+    }
+
+    boolean contains(T element) {
+        return this.data.contains(element);
+    }
+
+    private boolean contains(int i) {
+        return i >= 0 && i < this.data.size();
+    }
+
+    void swap(int i1, int i2) {
+        if (this.contains(i1) && this.contains(i2)) {
+            T val1 = this.data.get(i1);
+            T val2 = this.data.get(i2);
+            this.data.set(i1, val2);
+            this.data.set(i2, val1);
+        }
+    }
+
+    int countGraterThan(T e) {
+        int counter = 0;
+        for (int i = 0; i < this.data.size(); i++) {
+            T element = this.data.get(i);
+            if (e.compareTo(element) < 0) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    T getMax(){
+        return this.data.stream().max(Comparable::compareTo).get();
+    }
+    T getMin(){
+        return this.data.stream().min(Comparable::compareTo).get();
+    }
+    void print(){
+        for (T val : this.data) {
+            System.out.println(val);
+        }
+
+    }
+}
---
+++ Java Advanced/Generics/src/custom_list/Main.java
@@ -0,0 +1,45 @@
+package custom_list;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        CustomList<String> customList = new CustomList<>();
+        while (!"END".equals(line)) {
+            String[] elements = line.split("\\s+");
+            String command = elements[0];
+            switch (command) {
+                case "Add":
+                    customList.add(elements[1]);
+                    break;
+                case "Remove":
+                    customList.remove(Integer.parseInt(elements[1]));
+                    break;
+                case "Contains":
+                    System.out.println(customList.contains(elements[1]));
+                    break;
+                case "Swap":
+                    int i1 = Integer.parseInt(elements[1]);
+                    int i2 = Integer.parseInt(elements[2]);
+                    customList.swap(i1, i2);
+                    break;
+                case "Greater":
+                    System.out.println(customList.countGraterThan(elements[1]));
+                    break;
+                case "Max":
+                    System.out.println(customList.getMax());
+                    break;
+                case "Min":
+                    System.out.println(customList.getMin());
+                    break;
+                case "Print":
+                    customList.print();
+                    break;
+            }
+
+            line = scanner.nextLine();
+        }
+    }
+}
