570e33958476fd923b009f66d44e2de6e40a24fe
speeddemon <mikael90@abv.bg>
2019-10-19 14:14:55 +0300

    Added Generic_swap_method_strings task

---
+++ Java Advanced/Generics/src/generic_swap_method_strings/Main.java
@@ -0,0 +1,21 @@
+package generic_swap_method_strings;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Box<String> box = new Box<>();
+        for (int i = 0; i < n; i++) {
+            box.addValue(scanner.nextLine());
+        }
+        int swapElementIndex = scanner.nextInt();
+        int swapElementToIndex = scanner.nextInt();
+        box.swap(swapElementIndex,swapElementToIndex);
+        for (String value : box.getData()) {
+            System.out.println("java.lang.String: " + value);
+        }
+
+    }
+}
