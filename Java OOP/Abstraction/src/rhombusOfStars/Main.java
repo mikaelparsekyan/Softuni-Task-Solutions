533e63520b6977128dd53751300bac27b0c0a80e
speeddemon <mikael90@abv.bg>
2019-10-28 18:09:07 +0200

    Added RhombusOfStars task

---
+++ Java OOP/Abstraction/src/rhombusOfStars/Main.java
@@ -0,0 +1,32 @@
+package rhombusOfStars;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        printRombus(n);
+    }
+    private static void printRombus(int n){
+        //IF IT IS HARD TO READ, PROSTO ZAEBI...
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = n - i - 1; j >0 ; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
