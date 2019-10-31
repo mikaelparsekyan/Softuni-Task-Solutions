44861aaeaeb54d06a5411b64ca80201d5f96bd21
speeddemon <mikael90@abv.bg>
2019-10-31 14:38:30 +0200

    Fixed CardSuit task

--- Java OOP/Abstraction/src/cardSuit/Main.java
+++ Java OOP/Abstraction/src/cardSuit/Main.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("%s:%n",scanner.nextLine());
+        System.out.printf("%s:%n", scanner.nextLine());
         Cards[] cards = Cards.values();
-        for (int i = 0; i < cards.length; i++) {
+        for (Cards card : cards) {
             System.out.printf("Ordinal value: %d; Name value: %s%n",
-                    i,cards[i]);
+                    card.ordinal(), card.toString());
         }
     }
 }
