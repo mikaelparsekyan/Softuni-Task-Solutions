c4ee15a9fff6cdb8d40978b5e52c2307f63159f6
speeddemon <mikael90@abv.bg>
2019-12-21 14:19:46 +0200

    Fixed create_tables.sql 100/100

--- Java DB/Data Definition And Data Types/Exercises/create_tables.sql
+++ Java DB/Data Definition And Data Types/Exercises/create_tables.sql
@@ -1,5 +1,9 @@
-CREATE TABLE `minions`.`minions` (
+CREATE TABLE `minions` (
   `id` INT NOT NULL,
   `name` VARCHAR(255) NULL,
   `age` INT NULL,
+  PRIMARY KEY (`id`));
+CREATE TABLE `towns` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(255) NULL,
   PRIMARY KEY (`id`));
