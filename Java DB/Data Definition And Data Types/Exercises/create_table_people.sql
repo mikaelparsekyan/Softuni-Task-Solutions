7210fe4e0d2eb5d3d345217477a9fceb1daa3c18
speeddemon <mikael90@abv.bg>
2019-12-22 19:35:35 +0200

    Added create_table_people.sql query

---
+++ Java DB/Data Definition And Data Types/Exercises/create_table_people.sql
@@ -0,0 +1,28 @@
+CREATE TABLE `people`(
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL,
+    picture LONGBLOB,
+    height DECIMAL(10,2),
+    weight DECIMAL(10,2),
+    gender CHAR(1) NOT NULL,
+    birthdate TEXT NOT NULL,
+    biography TEXT
+);
+INSERT INTO `people` VALUES(
+NULL,"Kiro",NULL,15.43,15.43,"m","asd","piqnica"
+);
+INSERT INTO `people` VALUES(
+NULL,"Kiro",NULL,15.43,15.43,"m","asd","piqnica"
+);
+INSERT INTO `people` VALUES(
+NULL,"Kiro",NULL,15.43,15.43,"m","asd","piqnica"
+);
+INSERT INTO `people` VALUES(
+NULL,"Kiro",NULL,15.43,15.43,"m","asd","piqnica"
+);
+INSERT INTO `people` VALUES(
+NULL,"Kiro",NULL,15.43,15.43,"m","asd","piqnica"
+);
+-- dread :D
+
+ 
