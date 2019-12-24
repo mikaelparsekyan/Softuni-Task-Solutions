35285540cdbf0f51dcfe0fc629969bb13a20877f
speeddemon <mikael90@abv.bg>
2019-12-24 10:57:11 +0200

    Fixed insert_records_in_tables.sql query

--- Java DB/Data Definition And Data Types/Exercises/insert_records_in_tables.sql
+++ Java DB/Data Definition And Data Types/Exercises/insert_records_in_tables.sql
@@ -1,9 +1,9 @@
-INSERT INTO `towns` VALUES(1,"Sofia");
-INSERT INTO `towns` VALUES(2,"Plovdiv");
-INSERT INTO `towns` VALUES(3,"Varna");
+INSERT INTO `towns` VALUES(1,"Sofia"),
+	(2,"Plovdiv"),
+	(3,"Varna");
 
-INSERT INTO `minions` VALUES(1,"Kevin", 22, 1);
-INSERT INTO `minions` VALUES(2,"Bob", 15, 3);
-INSERT INTO `minions` VALUES(3,"Steward", NULL, 2);
+INSERT INTO `minions` VALUES(1,"Kevin", 22, 1),
+ 	(2,"Bob", 15, 3),
+ 	(3,"Steward", NULL, 2);
 
  
