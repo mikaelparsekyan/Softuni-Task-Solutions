d70ba2a8f181f8cfad15b1efc0f970a0ab66cfbf
speeddemon <mikael90@abv.bg>
2019-12-24 11:53:25 +0200

    Fixed create_table_people.sql query

--- Java DB/Data Definition And Data Types/Exercises/create_table_people.sql
+++ Java DB/Data Definition And Data Types/Exercises/create_table_people.sql
@@ -1,28 +1,15 @@
 CREATE TABLE `people`(
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(200) NOT NULL,
-    picture LONGBLOB,
-    height DECIMAL(10,2),
-    weight DECIMAL(10,2),
-    gender CHAR(1) NOT NULL,
-    birthdate TEXT NOT NULL,
-    biography TEXT
+	name VARCHAR(200) NOT NULL,
+	picture LONGBLOB,
+	height DECIMAL(10,2),
+	weight DECIMAL(10,2),
+	gender CHAR(1) NOT NULL,
+	birthdate TEXT NOT NULL,
+	biography TEXT
 );
-INSERT INTO `people` VALUES(
-NULL,"Kiro",NULL,15.43,15.43,"m","asd","piqnica"
-);
-INSERT INTO `people` VALUES(
-NULL,"Kiro",NULL,15.43,15.43,"m","asd","piqnica"
-);
-INSERT INTO `people` VALUES(
-NULL,"Kiro",NULL,15.43,15.43,"m","asd","piqnica"
-);
-INSERT INTO `people` VALUES(
-NULL,"Kiro",NULL,15.43,15.43,"m","asd","piqnica"
-);
-INSERT INTO `people` VALUES(
-NULL,"Kiro",NULL,15.43,15.43,"m","asd","piqnica"
-);
--- dread :D
-
- 
+INSERT INTO `people` VALUES(NULL,"Kiro",NULL,15.43,15.43,"m","asd","piqnica"),
+	(NULL,"Steven",NULL,15.43,15.43,"m","asd","piqnica"),
+	(NULL,"Tom",NULL,15.43,15.43,"m","asd","piqnica"),
+	(NULL,"Ben",NULL,15.43,15.43,"m","asd","piqnica"),
+	(NULL,"Mitio",NULL,15.43,15.43,"m","asd","piqnica");
