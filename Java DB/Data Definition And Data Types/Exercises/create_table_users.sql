57dd99006b07a2c493c75904916cc871f89c9d0b
speeddemon <mikael90@abv.bg>
2019-12-24 11:53:07 +0200

    Fixed create_table_users.sql query

--- Java DB/Data Definition And Data Types/Exercises/create_table_users.sql
+++ Java DB/Data Definition And Data Types/Exercises/create_table_users.sql
@@ -1,15 +1,15 @@
 CREATE TABLE `users`(
 	id INTEGER PRIMARY KEY AUTO_INCREMENT UNIQUE,
-    username VARCHAR(30) NOT NULL,
-    password VARCHAR(26) NOT NULL,
-    profile_picture BLOB,
-    last_login_time DATETIME,
+   username VARCHAR(30) NOT NULL,
+   password VARCHAR(26) NOT NULL,
+   profile_picture BLOB,
+   last_login_time DATETIME,
 	is_deleted BOOLEAN
 );
-INSERT INTO `users` VALUES(1,"John","1234",NULL,NULL,false);
-INSERT INTO `users` VALUES(2,"Mike","1234",NULL,NULL,false);
-INSERT INTO `users` VALUES(3,"Chris","1234",NULL,NULL,false);
-INSERT INTO `users` VALUES(4,"Ken","1234",NULL,NULL,false);
-INSERT INTO `users` VALUES(5,"Ben","1234",NULL,NULL,false);
+INSERT INTO `users` VALUES(1,"John","1234",NULL,NULL,FALSE),
+	(2,"Mike","1234",NULL,NULL,FALSE),
+	(3,"Chris","1234",NULL,NULL,FALSE),
+	(4,"Ken","1234",NULL,NULL,FALSE),
+	(5,"Ben","1234",NULL,NULL,FALSE);
 
  
