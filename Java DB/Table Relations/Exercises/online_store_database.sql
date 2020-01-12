13b3245367fedb227db314dd5137eb27ea5770a5
speeddemon <mikael90@abv.bg>
2020-01-12 17:37:59 +0200

    Added online_store_database.sql query

---
+++ Java DB/Table Relations/Exercises/online_store_database.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS `mytestdb`.`cities`, `mytestdb`.`customers`, `mytestdb`.`item_types`, `mytestdb`.`items`, `mytestdb`.`order_items`, mytestdb.orders;
+CREATE TABLE `cities`(
+	city_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+CREATE TABLE `customers`(
+	customer_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    birthday DATE,
+    city_id INT(11),
+    CONSTRAINT FOREIGN KEY (city_id) REFERENCES cities(city_id)
+);
+CREATE TABLE `item_types`(
+	item_type_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+CREATE TABLE `items`(
+	item_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    item_type_id INT(11),
+    CONSTRAINT FOREIGN KEY (item_type_id) REFERENCES item_types(item_type_id)
+);
+CREATE TABLE `orders`(
+	order_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+	customer_id INT(11),
+    CONSTRAINT FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+CREATE TABLE `order_items`(
+	order_id INT(11),
+	item_id INT(11),
+    CONSTRAINT PRIMARY KEY (order_id, item_id),
+    CONSTRAINT FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    CONSTRAINT FOREIGN KEY (item_id) REFERENCES items(item_id)
+);
