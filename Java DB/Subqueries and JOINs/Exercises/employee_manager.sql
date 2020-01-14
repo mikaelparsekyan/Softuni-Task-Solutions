7c0d64b1c75749dcb2492517342a10766e2e7112
speeddemon <mikael90@abv.bg>
2020-01-14 09:31:58 +0200

    Added employee_manager.sql query

---
+++ Java DB/Subqueries and JOINs/Exercises/employee_manager.sql
@@ -0,0 +1,6 @@
+SELECT e.employee_id, e.first_name, e.manager_id, m.first_name AS 'manager_name'
+FROM `employees` AS e
+JOIN `employees` AS m
+ON m.employee_id = e.manager_id
+WHERE e.manager_id = 3 OR e.manager_id = 7
+ORDER BY e.first_name ASC;
