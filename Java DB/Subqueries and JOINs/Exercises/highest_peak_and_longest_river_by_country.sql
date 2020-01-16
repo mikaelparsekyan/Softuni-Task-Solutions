65031384dbee1511a334997bb4215603d848ebf9
speeddemon <mikael90@abv.bg>
2020-01-16 10:10:36 +0200

    Added query

--- Java DB/Subqueries and JOINs/Exercises/highest_peak_and_longest_river_by_country.sql
+++ Java DB/Subqueries and JOINs/Exercises/highest_peak_and_longest_river_by_country.sql
@@ -1,4 +1,10 @@
-SELECT COUNT(*) - (
-	SELECT COUNT(DISTINCT country_code) FROM `mountains_countries` AS mc
-) AS country_count
-FROM `countries` AS c
+SELECT country_name, MAX(p.elevation) AS highest_peak_elevation, 
+r.length AS longest_river_length
+FROM `countries` as c
+LEFT JOIN `mountains_countries` AS mc ON c.country_code = mc.country_code
+LEFT JOIN `peaks` AS p ON p.mountain_id = mc.mountain_id
+LEFT JOIN `countries_rivers` AS cr ON c.country_code = cr.country_code
+LEFT JOIN `rivers` AS r ON r.id = cr.river_id
+GROUP BY country_name
+ORDER BY highest_peak_elevation DESC, country_name ASC
+LIMIT 5;
