b43e93fed1edce50780f463991bf0801e2667299
speeddemon <mikael90@abv.bg>
2020-01-19 22:50:45 +0200

    Added query

---
+++ Java DB/Database Programmability/Exercises/employees_with_salary_above_number.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE usp_get_employees_salary_above(sal DECIMAL(19,4))
+  BEGIN
+    SELECT
+      first_name,
+      last_name
+    FROM `employees`
+    WHERE salary >= sal
+    ORDER BY first_name ASC, last_name ASC, employee_id ASC;
+  END;
