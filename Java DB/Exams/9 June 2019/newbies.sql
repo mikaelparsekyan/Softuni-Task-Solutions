9c504690a544503cffa5e9c513aaea7d1dca008f
speeddemon <mikael90@abv.bg>
2020-01-22 11:04:44 +0200

    Added newbies.sql

---
+++ Java DB/Exams/9 June 2019/newbies.sql
@@ -0,0 +1,8 @@
+SELECT
+  id,
+  CONCAT(first_name, ' ', last_name) AS full_name,
+  CONCAT('$', salary)                AS salary,
+  started_on
+FROM employees
+WHERE salary >= 100000 AND started_on >= '2018-01-01'
+ORDER BY salary DESC, id ASC
