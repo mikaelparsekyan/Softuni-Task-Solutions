4ae20ecd78b4fd76037dc7fc244139c78cd30dea
speeddemon <mikael90@abv.bg>
2020-01-31 11:42:39 +0200

    Added delete.sql

--- Java DB/Exams/9 June 2019/delete.sql
+++ Java DB/Exams/9 June 2019/delete.sql
@@ -1,2 +1,4 @@
-DELETE FROM `employees`
-WHERE id NOT IN(SELECT employee_id FROM `employees_clients`)
+DELETE FROM employees
+WHERE
+  id NOT IN (SELECT employee_id
+               FROM employees_clients)
