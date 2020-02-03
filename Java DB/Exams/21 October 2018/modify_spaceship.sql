ef6fd910c262bda41b4e73d9ffa303949c994226
speeddemon <mikael90@abv.bg>
2020-02-03 19:46:47 +0200

    Added modify_spaceship.sql

---
+++ Java DB/Exams/21 October 2018/modify_spaceship.sql
@@ -0,0 +1,11 @@
+DELIMITER //
+CREATE PROCEDURE udp_modify_spaceship_light_speed_rate(spaceship_name VARCHAR(50), 
+light_speed_rate_increse INT(11))
+	BEGIN
+		IF spaceship_name IN(SELECT name FROM spaceships)
+			THEN
+				UPDATE spaceships SET light_speed_rate = light_speed_rate + light_speed_rate_increse;
+		ELSE
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Spaceship you are trying to modify does not exists.';
+        END IF;
+    END;
