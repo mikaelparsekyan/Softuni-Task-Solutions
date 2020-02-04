3c361c30250e4d2026ae8b2bd0c148d88782972a
speeddemon <mikael90@abv.bg>
2020-02-04 18:11:11 +0200

    Fixed query

--- Java DB/Database Programmability/Exercises/money_transfer.sql
+++ Java DB/Database Programmability/Exercises/money_transfer.sql
@@ -2,15 +2,16 @@ DELIMITER \\
 CREATE PROCEDURE usp_transfer_money(from_account_id INT, to_account_id INT, amount DECIMAL(19, 4))
 	BEGIN
 		START TRANSACTION;
-		IF amount > 0
-			THEN
-				UPDATE accounts
-                SET balance = balance - amount
-                WHERE id = from_account_id AND balance >= amount;
-                
-                UPDATE accounts
-                SET balance = balance + amount
-                WHERE id = to_account_id;
-		END IF;
+			IF amount > 0 AND from_account_id IN(SELECT id FROM accounts) 
+			AND to_account_id IN(SELECT id FROM accounts) AND amount <= (SELECT balance FROM accounts WHERE id = from_account_id LIMIT 1)
+				THEN
+					UPDATE accounts AS a
+					SET balance = balance - amount
+					WHERE id = from_account_id;
+					
+					UPDATE accounts
+					SET balance = balance + amount
+					WHERE id = to_account_id;
+			END IF;
         COMMIT;
     END;
