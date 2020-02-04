a8eca071486c16dd4c358c21a34d06b98ed0a283
speeddemon <mikael90@abv.bg>
2020-02-04 12:04:01 +0200

    Fixed people_with_balance_higher_than.sql

--- Java DB/Database Programmability/Exercises/people_with_balance_higher_than.sql
+++ Java DB/Database Programmability/Exercises/people_with_balance_higher_than.sql
@@ -2,7 +2,11 @@ DELIMITER //
 CREATE PROCEDURE usp_get_holders_with_balance_higher_than(num DECIMAL(19, 4))
 	BEGIN
 		SELECT first_name, last_name FROM account_holders AS ah
-        JOIN accounts AS a ON a.account_holder_id = ah.id
-        WHERE a.balance > num
-        ORDER BY ah.id ASC;
+        JOIN (
+			SELECT id, account_holder_id, SUM(balance) AS 'total_balance' FROM accounts
+            GROUP BY account_holder_id
+            HAVING total_balance > num
+            ORDER BY id
+        ) AS a ON a.account_holder_id = ah.id
+        ORDER BY a.id ASC;
     END;
