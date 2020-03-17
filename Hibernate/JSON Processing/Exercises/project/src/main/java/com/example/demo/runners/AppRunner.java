d568441c7ebe6762d8b7f8f5406724ef471226f0
speeddemon <mikael90@abv.bg>
2020-03-17 13:23:25 +0200

    Organizing files

---
+++ Hibernate/JSON Processing/Exercises/project/src/main/java/com/example/demo/runners/AppRunner.java
@@ -0,0 +1,18 @@
+package com.example.demo.runners;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Component
+public class AppRunner implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
