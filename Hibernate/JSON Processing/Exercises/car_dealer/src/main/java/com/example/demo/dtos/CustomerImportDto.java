7cf5f86c5cbacce8fb2fbd719ebbbbb27ba45504
speeddemon <mikael90@abv.bg>
2020-03-19 22:29:12 +0200

    Added changes

---
+++ Hibernate/JSON Processing/Exercises/car_dealer/src/main/java/com/example/demo/dtos/CustomerImportDto.java
@@ -0,0 +1,19 @@
+package com.example.demo.dtos;
+
+import com.google.gson.annotations.Expose;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class CustomerImportDto {
+
+    @Expose
+    private String name;
+
+    @Expose
+    private String birthDate;
+
+    @Expose
+    private boolean isYoungDriver;
+}
--- Hibernate/JSON Processing/Exercises/car_dealer/src/main/java/com/example/demo/service/impl/CustomerServiceImpl.java
+++ Hibernate/JSON Processing/Exercises/car_dealer/src/main/java/com/example/demo/service/impl/CustomerServiceImpl.java
@@ -3,36 +3,53 @@ package com.example.demo.service.impl;
 import com.example.demo.constant.FileImportPath;
 import com.example.demo.data.dao.CustomerRepository;
 import com.example.demo.data.entities.Customer;
+import com.example.demo.dtos.CustomerImportDto;
 import com.example.demo.service.api.CustomerService;
 import com.example.demo.util.FileUtil;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
     @Autowired
     private final CustomerRepository customerRepository;
-
     @Autowired
     private final Gson gson;
+    @Autowired
+    private final ModelMapper modelMapper;
 
-    public CustomerServiceImpl(CustomerRepository customerRepository, Gson gson) {
+    public CustomerServiceImpl(CustomerRepository customerRepository, Gson gson, ModelMapper modelMapper) {
         this.customerRepository = customerRepository;
         this.gson = gson;
+        this.modelMapper = modelMapper;
     }
 
     @Override
     public void seedCustomersToDatabase() {
         String file = FileUtil.readFile(FileImportPath.CUSTOMERS_FILE_PATH);
 
-        List<Customer> customers = gson.fromJson(file, new TypeToken<List<Customer>>() {
-        }.getType());
+        List<CustomerImportDto> customers = gson
+                .fromJson(file, new TypeToken<List<CustomerImportDto>>() {
+                }.getType());
 
         if (customers != null) {
-            customers.forEach(customerRepository::saveAndFlush);
+            customers.stream().map(this::mapCustomer
+            ).forEach(customerRepository::saveAndFlush);
         }
+    }
+
+    private Customer mapCustomer(CustomerImportDto customerImportDto) {
+        Customer customer = modelMapper.map(customerImportDto, Customer.class);
+        customer.setBirthDate(LocalDate.parse(customerImportDto.getBirthDate()
+                        .replace("T", " "),
+                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        return customer;
     }
 }
